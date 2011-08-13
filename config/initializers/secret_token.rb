7da3cadc5d8b38408ca0b7f46bfc78faa7f63052
Jerel Miller <jermille@mines.edu>
2011-08-12 23:09:22 -0600

    First commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+BenHarris::Application.config.secret_token = '264cf952d8b1b075deb73ba9e603d3370b69beffa4875c862146ae2966f8e2f9eba4d1865d301f7e5b4a5ad68661f21ef75336da355f102f0f0196bc10108b23'
